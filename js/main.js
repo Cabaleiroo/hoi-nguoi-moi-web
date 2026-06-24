63cad51e32599ba4b33c40d2c827013d94c5747e
Cabaleiroo <141260228+Cabaleiroo@users.noreply.github.com>
2026-06-25 02:23:41 +0700

    Update the 1st time

--- js/main.js
+++ js/main.js
@@ -98,3 +98,19 @@ document.querySelectorAll("a").forEach((link) => {
     }, 400);
   });
 });
+
+const menuBtn = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
